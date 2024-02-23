8aaec08807bd7461fb2a8d95389d9f89215dd683
Abdullah Moursi <moursiabdullah@gmail.com>
2024-02-23 17:21:37 +0200

    adding loader

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,5 +1,6 @@
-import axios from "axios";
 import React, { FC, useState } from "react";
+import axios from "axios";
+import { FallingLines } from "react-loader-spinner";
 
 const baseURL = "http://api.exchangerate.host/timeframe?";
 const ACCESS_KEY = "b8e97526bd00a09c25a0b8705fd36069";
@@ -14,12 +15,12 @@ interface DateRangePickerProps {
 const DatePicker: FC<DateRangePickerProps> = ({ onDateRangeChange }) => {
   const [startDate, setStartDate] = useState<string | null>(null);
   const [endDate, setEndDate] = useState<string | null>(null);
-  const [exchangeData, setExchangeData] = useState<{} | null>({})
+  const [exchangeData, setExchangeData] = useState<any | null>({});
+  const [loader, setLoader] = useState<boolean>(false);
 
   const handleStartDateChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    console.log("change", event.target.value);
     const date = event.target.value ? event.target.value : null;
     setStartDate(date);
     if (onDateRangeChange) onDateRangeChange(date, endDate);
@@ -32,6 +33,7 @@ const DatePicker: FC<DateRangePickerProps> = ({ onDateRangeChange }) => {
   };
 
   const getExchangeData = async () => {
+    setLoader(true);
     const params = {
       start_date: startDate,
       end_date: endDate,
@@ -40,7 +42,7 @@ const DatePicker: FC<DateRangePickerProps> = ({ onDateRangeChange }) => {
     try {
       const response = await axios.get(baseURL, { params });
       setExchangeData(response.data);
-      console.log(exchangeData);
+      setLoader(false);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -50,29 +52,54 @@ const DatePicker: FC<DateRangePickerProps> = ({ onDateRangeChange }) => {
     if (startDate && endDate) {
       getExchangeData();
     }
-    alert('Please enter date values')
+    // alert('Please enter date values')
   };
 
   return (
-    <div>
-      <label htmlFor="startDate">Start Date:</label>
-      <input
-        type="date"
-        id="startDate"
-        value={startDate ? startDate.toString().split("T")[0] : ""}
-        onChange={handleStartDateChange}
-      />
+    <>
+      <div>
+        <label htmlFor="startDate">Start Date:</label>
+        <input
+          type="date"
+          id="startDate"
+          value={startDate ? startDate.toString().split("T")[0] : ""}
+          onChange={handleStartDateChange}
+        />
 
-      <label htmlFor="endDate">End Date:</label>
-      <input
-        type="date"
-        id="endDate"
-        value={endDate ? endDate.toString().split("T")[0] : ""}
-        onChange={handleEndDateChange}
-        min={startDate ? startDate.toString().split("T")[0] : ""}
-      />
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
+        <label htmlFor="endDate">End Date:</label>
+        <input
+          type="date"
+          id="endDate"
+          value={endDate ? endDate.toString().split("T")[0] : ""}
+          onChange={handleEndDateChange}
+          min={startDate ? startDate.toString().split("T")[0] : ""}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      {loader && !exchangeData.quotes && (
+        <FallingLines color="#4fa94d" width="100" visible={true} />
+      )}
+      {exchangeData.quotes && (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>EGP</th>
+              <th>CAD</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(exchangeData.quotes).map((item: any) => (
+              <tr key={item}>
+                <td>{item}</td>
+                <td>{exchangeData.quotes[item].USDEGP}</td>
+                <td>{exchangeData.quotes[item].USDCAD}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 };
 
