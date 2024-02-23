82e5ffcad8b652554ff137e7bbfea5f39db977a8
Abdullah Moursi <moursiabdullah@gmail.com>
2024-02-23 15:14:22 +0200

    setup axios request

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,31 +1,56 @@
+import axios from "axios";
 import React, { FC, useState } from "react";
 
+const baseURL = "http://api.exchangerate.host/timeframe?";
 const ACCESS_KEY = "b8e97526bd00a09c25a0b8705fd36069";
 
 interface DateRangePickerProps {
-  onDateRangeChange?: (startDate: Date | null, endDate: Date | null) => void;
+  onDateRangeChange?: (
+    startDate: string | null,
+    endDate: string | null
+  ) => void;
 }
 
 const DatePicker: FC<DateRangePickerProps> = ({ onDateRangeChange }) => {
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [startDate, setStartDate] = useState<string | null>(null);
+  const [endDate, setEndDate] = useState<string | null>(null);
+  const [exchangeData, setExchangeData] = useState<{} | null>({})
 
   const handleStartDateChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const date = event.target.value ? new Date(event.target.value) : null;
+    console.log("change", event.target.value);
+    const date = event.target.value ? event.target.value : null;
     setStartDate(date);
     if (onDateRangeChange) onDateRangeChange(date, endDate);
   };
 
   const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const date = event.target.value ? new Date(event.target.value) : null;
+    const date = event.target.value ? event.target.value : null;
     setEndDate(date);
     if (onDateRangeChange) onDateRangeChange(startDate, date);
   };
 
+  const getExchangeData = async () => {
+    const params = {
+      start_date: startDate,
+      end_date: endDate,
+      access_key: ACCESS_KEY,
+    };
+    try {
+      const response = await axios.get(baseURL, { params });
+      setExchangeData(response.data);
+      console.log(exchangeData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   const handleSubmit = () => {
-    console.log(startDate, endDate);
+    if (startDate && endDate) {
+      getExchangeData();
+    }
+    alert('Please enter date values')
   };
 
   return (
@@ -34,7 +59,7 @@ const DatePicker: FC<DateRangePickerProps> = ({ onDateRangeChange }) => {
       <input
         type="date"
         id="startDate"
-        value={startDate ? startDate.toISOString().split("T")[0] : ""}
+        value={startDate ? startDate.toString().split("T")[0] : ""}
         onChange={handleStartDateChange}
       />
 
@@ -42,9 +67,9 @@ const DatePicker: FC<DateRangePickerProps> = ({ onDateRangeChange }) => {
       <input
         type="date"
         id="endDate"
-        value={endDate ? endDate.toISOString().split("T")[0] : ""}
+        value={endDate ? endDate.toString().split("T")[0] : ""}
         onChange={handleEndDateChange}
-        min={startDate ? startDate.toISOString().split("T")[0] : ""}
+        min={startDate ? startDate.toString().split("T")[0] : ""}
       />
       <button onClick={handleSubmit}>Submit</button>
     </div>
